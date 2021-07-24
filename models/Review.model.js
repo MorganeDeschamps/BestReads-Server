e43accc95ba07ba803ff186676d669576a91cd7b
Morgane Deschamps <morgane.deschamps@gmail.com>
2021-07-24 18:10:50 +0200

    building the reviews

--- models/Review.model.js
+++ models/Review.model.js
@@ -1,7 +1,10 @@
 const { Schema, model } = require('mongoose');
+
 const reviewSchema = new Schema({
     owner: { type: Schema.Types.ObjectId, ref: 'User' },
-    comment: { type: String, required: true, maxlength: 280 }
+    comment: { type: String, required: true },
+    bookId: { type: String, required: true }
 });
+
 const Review = model('Review', reviewSchema);
 module.exports = Review;
